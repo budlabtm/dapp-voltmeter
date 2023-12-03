4049dbe18792ff9bd34b10ddf3fc0d9784f89ec4
Ivan Paraskun <87276459+mehandes@users.noreply.github.com>
2023-12-03 14:36:44 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     packages=['desktop_client_hfr_voltage'],
     setup_requires=['setuptools'],
-    url='https://github.com/Institute-of-Nuclear-Physics/desktop-client-hfr-voltage',
+    url='https://github.com/budlabtm/desktop-client-hfr-voltage',
     description='Multi-Channel voltage flow analyzer',
     install_requires=[
         'PyQt6',
